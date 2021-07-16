c845c2e0aaa33e93a380ea3f0c42d730a4c15e7a
Caseywalker <casey.walker118@gmail.com>
2021-07-15 19:26:38 -0500

    Reformating

--- TheHeist/Program.cs
+++ TheHeist/Program.cs
@@ -2,38 +2,39 @@
 
 namespace TheHeist
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Plan Your Heist!");
+      Console.WriteLine("Plan Your Heist!");
 
-            Console.WriteLine("Enter team member's name: ");
-            var name = Console.ReadLine();
+      Console.WriteLine("Enter team member's name: ");
+      var name = Console.ReadLine();
 
-            Console.WriteLine("Enter " + name + "'s skill level: ");
-            var skill = Int32.Parse(Console.ReadLine());
+      Console.WriteLine("Enter " + name + "'s skill level: ");
+      var skill = Int32.Parse(Console.ReadLine());
 
-            if (skill < 0)
-            {
-                skill = 0;
-            }
+      if (skill < 0)
+      {
+        skill = 0;
+      }
 
-            Console.WriteLine("Enter " + name + "'s courage factor");
-            var courage = Decimal.Parse(Console.ReadLine());
+      Console.WriteLine("Enter " + name + "'s courage factor");
+      var courage = Decimal.Parse(Console.ReadLine());
 
-            if (courage < 0)
-            {
-                courage = 0m;
-            } else if (courage > 2.0m)
-            {
-                courage = 2.0m;
-            }
+      if (courage < 0)
+      {
+        courage = 0m;
+      }
+      else if (courage > 2.0m)
+      {
+        courage = 2.0m;
+      }
 
-            Console.WriteLine("Team Member's name is " + name);
-            Console.WriteLine(name + "'s skill level is " + skill);
-            Console.WriteLine(name + "'s courage factor is " + courage);
-     
-        }
+      Console.WriteLine("Team Member's name is " + name);
+      Console.WriteLine(name + "'s skill level is " + skill);
+      Console.WriteLine(name + "'s courage factor is " + courage);
+
     }
+  }
 }
